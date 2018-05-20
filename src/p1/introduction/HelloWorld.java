58ae47041ee496df5a18d4b7dcdf2377677cb784
Soumya R Mishra <smishra090@nam.ad.pwcinternal.com>
2018-05-20 23:16:31 +0530

    1st commit on 20th May,2018

---
+++ src/p1/introduction/HelloWorld.java
@@ -0,0 +1,18 @@
+/*This is a Program to display the input
+Author -Me
+*/
+
+package p1.introduction;
+
+import java.lang.String;
+import java.lang.System;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+	}
+
+}
